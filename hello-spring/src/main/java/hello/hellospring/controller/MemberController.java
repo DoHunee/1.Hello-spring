56b02b6bd8b68312cb4c90376c0f2bc473530d0f
장도훈 <jehgns1@gmail.com>
2024-07-06 19:07:38 +0900

    회원 웹 기능- 조회 구현 완료!!!

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -2,10 +2,16 @@ package hello.hellospring.controller;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
+
+import java.util.List;
+
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MemberController {
@@ -22,7 +28,6 @@ public class MemberController {
     return "members/createMemberForm";
   }
 
-
   // 회원 가입 처리 로직
   @PostMapping(value = "/members/new")
   public String create(MemberForm form) {
@@ -31,15 +36,18 @@ public class MemberController {
 
     // 입력한 이름을 콘솔로 확인할 수 있다!!
     System.out.println("member = " + member.getName());
-    
-    
+
     memberService.join(member);
     return "redirect:/"; // 처음 화면(루트경로)으로 돌아가게 하는것!
   }
 
-
-
-
+  
+  @GetMapping(value = "/members")
+  public String list(Model model) {
+    List<Member> members = memberService.findMembers();
+    model.addAttribute("members", members);
+    return "members/memberList";
+  }
 
 
 }
